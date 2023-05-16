30b8f459b23c2ff0132eed16a18715ec1419b6c4
M-D-HZ <Othman-hamzaoui@hotmail.com>
2023-05-16 17:20:23 +0200

    Change from svelte to react

---
+++ Front-end/portfolio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;

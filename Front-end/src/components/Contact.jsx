3e039b4d0c8f9c62e2830f08db294794c0d447f7
M-D-HZ <othman-hamzaoui@hotmail.com>
2024-02-07 23:16:25 +0100

    Small change

--- Front-end/src/components/Contact.jsx
+++ Front-end/src/components/Contact.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import {styles} from "../style.js";
+import SectionWrapper from "../hoc/sectionWrapper.jsx";
 
 const Contact = () => {
   return (
-      <div className={"xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden"}>
+      <div className={"xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden content-center"}>
         <div className={"font-medium "}>Othman-hamzaoui@hotmail.com</div>
       </div>
 
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact")
